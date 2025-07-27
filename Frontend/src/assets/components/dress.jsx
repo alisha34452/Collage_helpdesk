fa92d913fd2bc348392aa2a896679aeb0b6e25fd
AkshayGit <akshayrishu4@gmail.com>
2025-07-27 21:01:13 +0530

    fix: Remove unnecessary margin from Google Map container in Dress component

--- Frontend/src/assets/components/dress.jsx
+++ Frontend/src/assets/components/dress.jsx
@@ -13,7 +13,7 @@ const Dress = () => {
       </div>
 
       {/* Google Map */}
-      <div className="w-full md:max-w-[80vw] h-[450px] border-2 border-gray-300 !my-4 shadow-lg rounded-lg overflow-hidden">
+      <div className="w-full md:max-w-[80vw] h-[450px] border-2 border-gray-300 shadow-lg rounded-lg overflow-hidden">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3270.2742855589054!2d87.95101149999999!3d26.1041499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e516b8d15d9ebb%3A0xcca9bbe37fab703d!2sSHREE%20ANAND!5e1!3m2!1sen!2sin!4v1753630011842!5m2!1sen!2sin"
           title="Location Map"
